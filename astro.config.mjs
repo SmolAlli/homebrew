72209f0c4745035b3d93a260d9ba62e92c125ddc
Alli <allikat2706@gmail.com>
2024-05-27 18:35:55 +1000

    Add basic pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://smolalli.github.io',
-    base: 'homebrew',
+  site: 'https://smolalli.github.io',
+  base: 'homebrew',
+  integrations: [svelte()]
 });
